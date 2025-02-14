839a5f9476c232be0822bc6e74959c79dcc13d35
clara050310 <clara.hernandes0310@gmail>
2025-02-14 07:48:51 -0300

    14.02.25

---
+++ Exercicio2.2.js
@@ -0,0 +1,12 @@
+// 1. Criando a variável anoNascimento como número
+let anoNascimento = 2000;
+
+// 2. Convertendo anoNascimento para String e exibindo o tipo de dado
+let anoNascimentoString = String(anoNascimento);
+console.log("Tipo de anoNascimentoString:", typeof anoNascimentoString); // Deve exibir "string"
+
+// 3. Criando a variável peso como String e convertendo para número
+let peso = "80.5";
+let pesoNumero = Number(peso);
+console.log("Tipo de pesoNumero:", typeof pesoNumero); // Deve exibir "number"
+console.log("Valor de pesoNumero:", pesoNumero); // Deve exibir 80.5
