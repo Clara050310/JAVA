3f70f66eb27b6ea8be29b0889ad7204e81c4ee25
clara050310 <clara.hernandes0310@gmail>
2025-02-14 07:48:19 -0300

    14.02.25 Exercicio2

---
+++ Exercicio2.js
@@ -0,0 +1,16 @@
+// Definição das variáveis
+let nome = "Clara"; // String
+let idade = 17; // Number
+let altura = 1.7; // Number
+let programador = true; // Boolean
+
+// Exibindo no console
+console.log("Nome:", nome);
+console.log("Idade:", idade);
+console.log("Altura:", altura);
+console.log("É programador?", programador);
+
+//let nome = "Clara"; → Define uma variável nome do tipo String.
+//let idade = 20;→ Defina `ididade como um Number (número inteiro).
+//let altura = 1.65; → Define altura como um Number
+//let programador = true; (true ou false).
